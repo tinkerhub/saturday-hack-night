6ae41daa0ce20ef32c35c6000bb4739d2dba2178
Rohit <tprohit9@gmail.com>
2021-08-31 01:04:06 +0530

    added onNewUser function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,17 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
+const app = admin.initializeApp();
+const firestore = app.firestore();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const onNewUser = functions.auth.user().onCreate((user) =>
+{
+	const data = {
+		name: user.displayName,
+		uid: user.uid,
+		email: user.email,
+		avatar: user.photoURL,
+	};
+
+	return firestore.collection("users").doc(user.uid).set(data, {merge: true});
 });
