287d91eab5bc99e8173a59e28f5185b9d9db716c
Rohit <tprohit9@gmail.com>
2021-09-02 01:22:18 +0530

    added mail sending

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,78 @@ import fetch from "node-fetch";
 const app = admin.initializeApp();
 const firestore = app.firestore();
 
+async function setUpGitRepo(eventID: string, teamID:string, repoUrl:string, name:string)
+{
+	const token = functions.config().github.token;
+
+	const event = await firestore.doc(`events/${eventID}`).get();
+	const repo = `${event.get("org")}/${event.get("repoName")}`;
+
+	const response =
+		await fetch(`https://api.github.com/repos/${repo}/actions/workflows/add-project.yml/dispatches`,
+			{
+				headers:
+				{
+					"Accept": "application/vnd.github.v3+json",
+					"Authorization": `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+				method: "POST",
+				body: JSON.stringify(
+					{
+						ref: "main",
+						inputs: {
+							"repo_url": repoUrl,
+							"folder_name": `${name} | ${teamID}`,
+						},
+					}),
+			})
+			.then((res) => res.text());
+
+	if (response)
+		console.error(response);
+}
+
+async function queueMails(bulk: FirebaseFirestore.Transaction, eventID: string, teamID:string,
+	lead: string, members: Array<string>, teamName: string, repo: string)
+{
+	const mails = firestore.collection(`/events/${eventID}/teams/${teamID}/mails`);
+	const leadUser = await firestore.doc(`users/${lead}`).get();
+
+	bulk.create(mails.doc(lead), {
+		to: [leadUser.get("email")],
+		template: {
+			name: "create",
+			data: {
+				name: leadUser.get("name"),
+				teamName,
+				repo,
+				eventID,
+				teamID,
+			},
+		},
+	});
+
+	for (const member of members || [])
+	{
+		const user = await firestore.doc(`users/${member}`).get();
+
+		bulk.create(mails.doc(member), {
+			to: [user.get("email")],
+			template: {
+				name: "invite",
+				data: {
+					lead: leadUser.get("name"),
+					name: user.get("name"),
+					teamName,
+					eventID,
+					teamID,
+				},
+			},
+		});
+	}
+}
+
 export const onNewUser = functions.auth.user().onCreate((user) =>
 {
 	const data = {
@@ -21,61 +93,40 @@ export const onNewUser = functions.auth.user().onCreate((user) =>
 export const onTeamCreated = functions.firestore.document("/events/{eventID}/teams/{teamID}")
 	.onCreate(async (snapshot, context) =>
 	{
-		const bulk = firestore.bulkWriter();
-		const membersRef = snapshot.ref.collection("members");
+		return firestore.runTransaction(async (bulk) =>
+		{
+			const membersRef = snapshot.ref.collection("members");
 
-		bulk.create(firestore.doc(`users/${snapshot.get("lead")}/teams/${context.params.eventID}`), {
-			teamID: context.params.teamID,
-			eventID: context.params.eventID,
-			name: snapshot.get("name"),
-			repo: snapshot.get("repo"),
-			lead: true,
+			bulk.create(firestore.doc(`users/${snapshot.get("lead")}/teams/${context.params.eventID}`), {
+				teamID: context.params.teamID,
+				eventID: context.params.eventID,
+				name: snapshot.get("name"),
+				repo: snapshot.get("repo"),
+				lead: true,
+			});
+
+			if (snapshot.get("members"))
+				for (const uid of snapshot.get("members"))
+					bulk.create(membersRef.doc(uid),
+						{
+							uid,
+							accepted: false,
+						});
+
+			await setUpGitRepo(context.params.eventID, context.params.teamID,
+				snapshot.get("repo"), snapshot.get("name"));
+
+			await queueMails(bulk, context.params.eventID, context.params.teamID,
+				snapshot.get("lead"), snapshot.get("members"), snapshot.get("name"), snapshot.get("repo"));
 		});
-
-		if (snapshot.get("members"))
-			for (const uid of snapshot.get("members"))
-				bulk.create(membersRef.doc(uid),
-					{
-						uid,
-						inviteSent: false,
-						accepted: false,
-					});
-
-		const token = functions.config().github.token;
-
-		const event = await firestore.doc(`events/${context.params.eventID}`).get();
-		const repo = `${event.get("org")}/${event.get("repoName")}`;
-
-		const response =
-		await fetch(`https://api.github.com/repos/${repo}/actions/workflows/add-project.yml/dispatches`,
-			{
-				headers:
-				{
-					"Accept": "application/vnd.github.v3+json",
-					"Authorization": `Bearer ${token}`,
-					"Content-Type": "application/json",
-				},
-				method: "POST",
-				body: JSON.stringify(
-					{
-						ref: "main",
-						inputs: {
-							"repo_url": snapshot.get("repo"),
-							"folder_name": `${snapshot.get("name")} | ${context.params.teamID}`,
-						},
-					}),
-			})
-			.then((res) => res.text());
-
-		if (response)
-			console.error(response);
-
-		return bulk.flush();
 	});
 
 
 export const joinTeam = functions.https.onCall(async (data, context)=>
 {
+	if (!context.app)
+		throw new functions.https.HttpsError("failed-precondition", "App check failed");
+
 	if (!context.auth?.uid)
 		throw new functions.https.HttpsError("unauthenticated", "User not authenticated");
 
