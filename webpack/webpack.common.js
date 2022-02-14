43a1f2e7cab5c6d6e8bac7c670fd5c3bb1c70af5
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-02-14 22:22:47 +0530

    Fixes low res Images

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -33,11 +33,7 @@ module.exports = (env) => ({
                     {
                         loader: "responsive-loader",
                         options: {
-                            adapter: require("responsive-loader/sharp"),
-                            sizes: [320, 640, 960, 1200, 1800, 2400],
-                            placeholder: true,
-                            placeholderSize: 20,
-                            disabled: env === "dev"
+                            adapter: require("responsive-loader/sharp")
                         },
                     },
                 ],
