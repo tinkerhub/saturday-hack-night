fbeb62e8f7edd15276a4b52fc4c43e2ff4306166
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-02-19 15:01:29 +0530

    Fix MemberChips not getting removed

--- web/react/components/MemberChips/MemberChips.tsx
+++ web/react/components/MemberChips/MemberChips.tsx
@@ -1,5 +1,5 @@
 import { getDocs, query, collection, where } from "firebase/firestore";
-import React, { useState } from "react";
+import { useState } from "react";
 import { db } from "../../firebase";
 import "./MemberChips.css";
 import userIcon from "../../../assets/fallbacks/user.png";
@@ -10,51 +10,58 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 interface MemberChipProps {
     onChange: (uid: string, add: boolean) => void | undefined;
 }
+interface userData{
+    gId: string;
+    avatar?: string | null;
+    uid: string;
+}
 function MemberChips({ onChange }: MemberChipProps) 
 {
+    
     const [users, setUsers] = useState<string>("");
-    const [chips, setChips] = useState<{ [key: string]: { uid?: string, avatar?: string } }>({});
+    const [chips, setChips] = useState<Array<userData>> ([]);
     library.add(faTimes);
-    function deleteItem (chips: { [key: string]: { uid?: string, avatar?: string } }, gid: string)
-    {
-        if (chips[gid].uid && chips[gid].uid !== "invalid")
+    function deleteItem (gid: string, chips:Array<userData>):Array<userData>
+    {   
+        const item = chips.filter((value) => value.gId === gid)[0];
+        if(item && item.gId !== null)
         {
-            onChange(chips[gid].uid || "", false);
+            onChange(item.uid, false);
             console.log("Chip Removed");
-        } 
-        delete chips[gid];
+        }
+        chips = chips.filter((value)=> value.gId !== gid);
         return chips;
     }
-
     async function addUser()
     {
         const gitHubIds = users.replaceAll(/\s/g, "").split(" ");
-        const newChips: { [key: string]: { uid?: string, avatar?: string } } = {};
-
+        const newChips: Array<userData> = [];
         setUsers("");
-
         for (const gid of gitHubIds)
-            if (!(gid in chips) && gid !== "")
-                newChips[gid] = {};
-
-        setChips((chips) => ({...chips, ...newChips}));
-
-        for (const gid in newChips)
         
-            getDocs(query(collection(db, "users"), where("githubID", "==", gid))).then((snap) =>
+            if(!(chips.filter((item) => item.gId === gid).length > 0) && gid !== "")
+            
+                newChips.push({gId: gid, avatar:"", uid:""});
+            
+        
+        setChips(chips.concat(newChips));
+        newChips.forEach((item) => 
+        {
+            getDocs(query(collection(db, "users"), where("githubID", "==", item.gId))).then((snap) =>
             {
-                let user: { uid: string; avatar?: string; };
+                let user: userData;
                 if (snap.docs.length !== 0)
                 {
-                    user = {uid: snap.docs[0].id, avatar: snap.docs[0].get("avatar")};
+                    user = {gId: item.gId, uid: snap.docs[0].id, avatar: snap.docs[0].get("avatar")};
                     onChange(snap.docs[0].id, true);
                 }
                 else
-                    user = {uid: "invalid"};
-
-                setChips((chips) => ({...chips, [gid]: user}));
+                
+                    user = {gId:item.gId, uid: "invalid"};    
+                
+                setChips(chips.concat(user));
             });
-        
+        });
     }
     return(
         <>
@@ -72,15 +79,15 @@ function MemberChips({ onChange }: MemberChipProps)
             }}/>
             <div className="chipContainer">
                 {
-                    Object.keys(chips).map((gid)=>
+                    chips.map((user, key)=>
                     {
                         return(
-                            <div key={gid} className={chips[gid].uid === "invalid"? "invalid chip" : "valid chip"}>
-                                <img src={chips[gid].avatar? chips.gId.avatar :userIcon } alt="avatar" className="avatar"/>
-                                <span className="chipText">{gid}</span>
-                                <div role="button" tabIndex={0} className="chipClose" onClick={()=>setChips(deleteItem(chips, gid))} onKeyDown={()=>setChips(deleteItem(chips, gid))}>
+                            <div key={key} className={user.uid === "invalid"? "invalid chip" : "valid chip"}>
+                                <img src={user.avatar? user.avatar :userIcon } alt="avatar" className="avatar"/>
+                                <span className="chipText">{user.gId}</span>
+                                <span role="button" tabIndex={0} className="chipClose" onClick={()=>setChips(deleteItem(user.gId, chips))} onKeyDown={()=>setChips(deleteItem(user.gId, chips))}>
                                     <FontAwesomeIcon className="icon" icon="times"/>
-                                </div>
+                                </span>
                             </div>
                         );
                     })
