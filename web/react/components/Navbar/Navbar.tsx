1de63b8768229e80f5dc990cf0f7b533614f4c57
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-02-19 21:17:35 +0530

    Added Navbar for mobile viewport

--- web/react/components/Navbar/Navbar.tsx
+++ web/react/components/Navbar/Navbar.tsx
@@ -2,15 +2,21 @@ import "./Navbar.css";
 import logo from "../../../assets/logo.png";
 import fallbackAvatar from "../../../assets/fallbacks/user.png";
 import { auth } from "../../firebase";
-import { useEffect, useState } from "react";
+import { useEffect, useState, createRef } from "react";
 import { onAuthStateChanged, User, signInWithPopup, GithubAuthProvider, signOut } from "firebase/auth";
 import { Link } from "react-scroll";
 import { NavLink, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 function Navbar() 
 {
     const [user, setUser] = useState<User | null>(null);
+    const [toggle, setToggle] = useState<boolean>(false);
     const [userAvatar, setuserAvatar] = useState(fallbackAvatar);
+    const navRef = createRef<HTMLDivElement>();
     const navigate = useNavigate();
+    library.add(faBars);
     const provider = new GithubAuthProvider();
     useEffect(()=>
     {
@@ -29,8 +35,20 @@ function Navbar()
                 setUser(null);
             
         });
-    }, []);
-
+    });
+    useEffect(()=>
+    {
+        if(toggle)
+        {
+            navRef.current?.classList.toggle("navLinksActive");
+            navRef.current?.classList.remove("navLinks");
+        }
+        else
+        {
+            navRef.current?.classList.remove("navLinksActive");
+            navRef.current?.classList.add("navLinks");
+        }
+    }, [toggle, navRef]);
     return(
         <>
             <div className="navbar">
@@ -39,6 +57,9 @@ function Navbar()
                     <div className='brandName'>
                         &nbsp;Saturday HackNight
                     </div>
+                </div>
+                <div className="navIconCon" tabIndex={0} role="button" onClick={()=>setToggle(!toggle)} onKeyPress={()=>setToggle(!toggle)}>
+                    <FontAwesomeIcon icon="bars" className="navIcon" size='2x'/>
                 </div>
                 <div className="userStatus">
                     {
@@ -65,7 +86,7 @@ function Navbar()
                             </button>
                     }
                 </div>
-                <div className='navLinks'>
+                <div className='' ref={navRef}>
                     <Link className="navLink" to='home' spy={true} smooth={true} offset={-100} duration={500}>
                         HOME
                     </Link>
