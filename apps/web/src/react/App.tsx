6df2e462c41bf51641f5c8475d992990625e1f2d
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-08-19 17:16:45 +0530

    chore: setup client

---
+++ apps/web/src/react/App.tsx
@@ -0,0 +1,10 @@
+import { ChakraProvider, Heading } from '@chakra-ui/react';
+import React from 'react';
+
+const App = () => (
+    <ChakraProvider>
+        <Heading>Heeey</Heading>
+    </ChakraProvider>
+);
+
+export default App;
---
+++ apps/web/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            favicon: path.resolve(__dirname, 'src/assets/favicon.svg'),
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                removeComments: true,
+            },
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                loader: 'babel-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
