94137eb17f7ae98963f345a6bc8549e853571a34
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-08-28 23:19:19 +0530

    feat: add campus details

--- web/react/Routes/Profile/index.tsx
+++ web/react/Routes/Profile/index.tsx
@@ -3,7 +3,7 @@ import {Auth, GithubAuthProvider, signInWithPopup} from "firebase/auth";
 
 import React, {useEffect, useState} from "react";
 
-import {Container, Snackbar, TextField} from "@mui/material";
+import {Autocomplete, Container, Snackbar, TextField} from "@mui/material";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
@@ -24,15 +24,59 @@ interface ProfileProps
  * @author Rohit T P
  * @returns { JSX.Element } Event route
  */
+
+const districts = [
+    "Thiruvananthapuram",
+    "Kollam",
+    "Pathanamthitta",
+    "Kottayam",
+    "Alappuzha",
+    "Idukki",
+    "Ernakulam",
+    "Thrissur",
+    "Palakkad",
+    "Malappuram",
+    "Kozhikode",
+    "Wayanad",
+    "Kannur",
+    "Kasaragod",
+    "Other",
+];
+const tempCampus = [{"id":"1-7001613899", "name":"school of ocean engineering and underwater technology", "address":"kerala university of fisheries and ocean studies campus, panangad p.o,", "type":"government"}];
+interface userData {
+    phno: string;
+    email: string;
+    district: string;
+}
+const toTitleCase = str => str.replace(/(^\w|\s\w)(\S*)/g, (_, m1, m2) => m1.toUpperCase()+m2.toLowerCase());
+
 function Profile({auth, db}: ProfileProps): JSX.Element
 {
     const [user, setUser] = useState<DocumentSnapshot<DocumentData> | null>(null);
-    const [data, setData] = useState({phno: "", email: ""});
-    const [error, setError] = useState({phno: false, email: false});
+    const [data, setData] = useState<userData>({phno: "", email: "", district: ""});
+    const [campus, setCampus] = useState({id: "", name: ""});
+    const [error, setError] = useState({phno: false, email: false, district: false, campusID: false});
+    const [campuses, setCampuses] = useState(tempCampus);
     const [status, setStatus] = useState(0);
-
     const {back} = getParams(useLocation().search);
     const navigate = useNavigate();
+
+    useEffect(() => 
+    {
+        let loading = true;
+        fetchCampus(data.district);
+        return () => 
+        {
+            loading = false;
+        };
+        async function fetchCampus(district: string)
+        {
+            const req = await fetch(`https://us-central1-educational-institutions.cloudfunctions.net/getCollegeByDistrict?district=${district}`);
+            const res = await req.json();
+            if(!loading)  return; 
+            if (res) setCampuses(res);
+        }
+    }, [data.district]);
 
     useEffect(() =>
     {
@@ -44,7 +88,8 @@ function Profile({auth, db}: ProfileProps): JSX.Element
             {
                 const snap = await getDoc(doc(db, `users/${authUser.uid}`));
                 setUser(snap);
-                setData({phno: snap.get("phno"), email: snap.get("email")});
+                setData({phno: snap.get("phno"), email: snap.get("email"), district: snap.get("district") || ""});
+                setCampus({id: snap.get("campusID") || "", name: snap.get("campusName") || ""});
             }
         });
 
@@ -58,12 +103,21 @@ function Profile({auth, db}: ProfileProps): JSX.Element
         if (!data.phno || !data.phno.match(/^(\+\d{1,3})?\d{10}$/g))
             return setError((error) => ({...error, phno: true}));
 
+        if (!data.district || !districts.includes(data.district))
+            return setError((error) => ({...error, district: true}));
+        if (data.district === "Other")
+            setCampus({name: "", id: ""});
         if (!user)
             return;
 
-        setError({phno: false, email: false});
-
-        updateDoc(user.ref, data)
+        setError({phno: false, email: false, district: false, campusID: false});
+        updateDoc(user.ref, {
+            phno: data.phno,
+            email: data.email,
+            district: data.district,
+            campusID: campus.id,
+            campusName: campus.name,
+        })
             .then(() => setStatus(1))
             .then(() => back && navigate(back))
             .catch(() => setStatus(-1));
@@ -98,6 +152,41 @@ function Profile({auth, db}: ProfileProps): JSX.Element
                     value={data.phno}
                     onChange={({target}) => setData((data) => ({...data, phno: target.value}))}
                 />
+                <Autocomplete 
+                    disablePortal
+                    options={districts}
+                    value={data.district}
+                    onChange={(_event, value) => setData((data) => 
+                    {
+                        if (value)
+                            return {...data, district: value};
+                        return {...data, district: ""};
+                    })}
+                    renderInput={(params) => <TextField {...params} 
+                        sx={{marginBottom: "1.1rem", marginTop: "1.1rem"}}
+                        helperText={error.district ? "Please select a valid district" : ""}
+                        label="District" />}
+                />
+                {
+                    data.district !== "Other" && data.district !== "" &&
+                <Autocomplete
+                    disablePortal
+                    options={campuses}
+                    getOptionLabel={(option) => toTitleCase(option.name)}
+                    value={campus}
+                    onChange={(_event, value) => setCampus(() =>
+                    {
+                        if (value)
+                            return {name: value.name, id: value.id};
+                        return {name: "", id: ""};
+                    })}
+                    renderInput={(params) => <TextField {...params}
+                        sx={{marginBottom: "1.1rem", marginTop: "1.1rem", textTransform: "uppercase"}}
+                        helperText={error.campusID ? "Please select a valid campus" : ""}
+                        label="Campus" />}
+                />
+                }
+                
                 <Button variant="contained" onClick={SaveDetails}>Save</Button>
             </Paper>
         </Container>
