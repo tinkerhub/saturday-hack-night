7e39e57b5f74f9cf5b7d191efad1292f554298a8
FathimaNiswa <fathimaniswanazer@gmail.com>
2022-11-08 20:05:28 +0530

    fix:package lock json

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -80,7 +80,8 @@ const Navbar = () => {
                 }}
             >
                 <Flex alignItems="center" justifyContent="space-between">
-                    <NavLink to="/"><Image
+                    <NavLink to="/">
+                        <Image
                         src={logo}
                         height="32px"
                         transition="all 0.4s ease-in"
@@ -91,7 +92,8 @@ const Navbar = () => {
                         onMouseOut={() => {
                             imageRef.current!.src = logo;
                         }}
-                    /></NavLink>
+                    />
+                    </NavLink>
                     <HStack
                         gap="18px"
                         display={{
