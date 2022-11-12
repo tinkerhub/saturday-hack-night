b481f74da7431c0daf673d19e88d4058c65d40dc
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-11-12 14:39:32 +0530

    chore: migration

---
+++ apps/api/prisma/migrations/20221112082537_db/migration.sql
@@ -0,0 +1,107 @@
+-- CreateEnum
+CREATE TYPE "TeamMemberRole" AS ENUM ('MEMBER', 'LEADER');
+
+-- CreateEnum
+CREATE TYPE "ActivityMode" AS ENUM ('ONLINE', 'OFFLINE');
+
+-- CreateEnum
+CREATE TYPE "ActivityStatus" AS ENUM ('REGISTRATION', 'RESULT', 'PENDING');
+
+-- CreateEnum
+CREATE TYPE "PitchStatus" AS ENUM ('PENDING', 'ABSENT', 'PRESENT');
+
+-- CreateEnum
+CREATE TYPE "ProjectStatus" AS ENUM ('PENDING', 'WORKING', 'COMPLETED', 'DROP', 'BESTPROJECT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "authid" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "githubid" TEXT NOT NULL,
+    "name" TEXT,
+    "mobile" TEXT,
+    "avatar" TEXT NOT NULL,
+    "campus" TEXT,
+    "district" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "details" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "mode" "ActivityMode" NOT NULL DEFAULT 'ONLINE',
+    "status" "ActivityStatus" NOT NULL DEFAULT 'PENDING',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Activity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "repo" TEXT NOT NULL,
+    "inviteCode" TEXT NOT NULL,
+    "activityId" TEXT NOT NULL,
+    "pitchStatus" "PitchStatus" NOT NULL DEFAULT 'PENDING',
+    "projectStatus" "ProjectStatus" NOT NULL DEFAULT 'PENDING',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TeamMember" (
+    "id" TEXT NOT NULL,
+    "role" "TeamMemberRole" NOT NULL DEFAULT 'MEMBER',
+    "teamId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "activityId" TEXT NOT NULL,
+
+    CONSTRAINT "TeamMember_pkey" PRIMARY KEY ("teamId","userId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_authid_key" ON "User"("authid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_githubid_key" ON "User"("githubid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Activity_id_key" ON "Activity"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Team_id_key" ON "Team"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Team_inviteCode_key" ON "Team"("inviteCode");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TeamMember_id_key" ON "TeamMember"("id");
+
+-- AddForeignKey
+ALTER TABLE "Team" ADD CONSTRAINT "Team_activityId_fkey" FOREIGN KEY ("activityId") REFERENCES "Activity"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_activityId_fkey" FOREIGN KEY ("activityId") REFERENCES "Activity"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
