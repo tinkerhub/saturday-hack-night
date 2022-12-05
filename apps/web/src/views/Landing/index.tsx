ffa222c7f3b7ddbb2a6a762f2727a9b8096eb4a1
Anbarasu <anbarasun123@gmail.com>
2022-12-05 21:11:43 +0530

    feat: add stats

--- apps/web/src/views/Landing/index.tsx
+++ apps/web/src/views/Landing/index.tsx
@@ -4,5 +4,6 @@ import Queries from './Queries';
 import Hear from './Hear';
 import Faq from './Faq';
 import What from './What';
+import Stats from './Stats';
 
-export { Home, What, How, Queries, Hear, Faq };
+export { Home, What, How, Queries, Hear, Faq, Stats };
