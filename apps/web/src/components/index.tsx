837465bd8634d18e633c240936e0b6e0e14a5167
Anbarasu <anbarasun123@gmail.com>
2022-12-09 09:43:57 +0530

    BREAKING CHANGE: removed workbox support (#24)
    * fix: cache
    
    * fix: remove workbox
    
    * add manifest

--- apps/web/src/components/index.tsx
+++ apps/web/src/components/index.tsx
@@ -5,6 +5,5 @@ import ParallaxView from './ParallaxView';
 import Accordion from './Accordion';
 import CurrentEvent from './CurrentEvent';
 import EventCard from './EventCard';
-import UpdateApp from './UpdateApp';
 
-export { NavBar, Card, Footer, ParallaxView, Accordion, CurrentEvent, EventCard, UpdateApp };
+export { NavBar, Card, Footer, ParallaxView, Accordion, CurrentEvent, EventCard };
