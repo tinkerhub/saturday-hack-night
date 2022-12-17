de11e4d5f48796cb0a1a82fdd8216fe447e4bd2c
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-12-18 02:30:27 +0530

    feat: migration

---
+++ apps/api/prisma/migrations/20221217202718_dev/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - The `mode` column on the `Activity` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `status` column on the `Activity` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `pitchStatus` column on the `Team` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `projectStatus` column on the `Team` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `role` column on the `TeamMember` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Activity" DROP COLUMN "mode",
+ADD COLUMN     "mode" "ActivityMode" NOT NULL DEFAULT 'ONLINE',
+DROP COLUMN "status",
+ADD COLUMN     "status" "ActivityStatus" NOT NULL DEFAULT 'PENDING';
+
+-- AlterTable
+ALTER TABLE "Team" DROP COLUMN "pitchStatus",
+ADD COLUMN     "pitchStatus" "PitchStatus" NOT NULL DEFAULT 'PENDING',
+DROP COLUMN "projectStatus",
+ADD COLUMN     "projectStatus" "ProjectStatus" NOT NULL DEFAULT 'PENDING';
+
+-- AlterTable
+ALTER TABLE "TeamMember" ADD COLUMN     "checkedIn" BOOLEAN NOT NULL DEFAULT false,
+DROP COLUMN "role",
+ADD COLUMN     "role" "TeamMemberRole" NOT NULL DEFAULT 'MEMBER';
